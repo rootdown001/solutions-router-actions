8c85fc1b084648e33686474ccbf1c1dc8e3f6124
Lance A. MD <lance.a.585@gmail.com>
2023-11-05 18:06:31 -0800

    Add files via upload

---
+++ src/TodoList.jsx
@@ -0,0 +1,35 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { TodoItem } from "./TodoItem";
+
+export function TodoList() {
+  const todos = useLoaderData();
+
+  return (
+    <div className="container">
+      <h1 className="page-title mb-2">
+        Todos
+        <div className="title-btns">
+          <Link to="/new" className="btn">
+            New
+          </Link>
+        </div>
+      </h1>
+
+      <form className="form">
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="query">Search</label>
+            <input type="search" name="query" id="query" />
+          </div>
+          <button className="btn">Search</button>
+        </div>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
